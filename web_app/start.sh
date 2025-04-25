b9ae95e6c7da026f3476263f2909d6de4c03161b
AnnaTheYe <azy2006@nyu.edu>
2025-04-25 01:03:05 -0400

    dropletizing everything

--- web_app/start.sh
+++ web_app/start.sh
@@ -2,5 +2,5 @@
 
 pip install --no-cache-dir --use-pep517 --progress-bar off -r runtime-requirements.txt
 
-./wait-for-it.sh mongodb:27017 --timeout=300 --strict -- \
+./wait-for-it.sh mongodb:27017 --timeout=90 --strict -- \
     gunicorn -c gunicorn_config.py wsgi:application
